df41865b0a8790c5f6f1988b0f28f3558e8ba89c
Kaozhi Chen <kylerchen16@gmail.com>
2024-03-11 17:54:11 -0400

    navbar

--- little-lemon/src/components/Navbar.jsx
+++ little-lemon/src/components/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/Logo.svg';
 import Hamburger from '../assets/hamburger.svg';
+import Close from '../assets/x-mark.svg';
 
 const Navbar = () => {
+  const [showMenu, setMenu] = useState(true);
+
+  const clickMenu = () => {
+    setMenu(!showMenu);
+  };
   return (
-    <div className='flex justify-between items-center p-4'>
+    <div className='flex justify-between items-center p-4 max-w-[1240px] mx:auto h-24 text-1'>
       <img src={Logo} alt='logo' />
 
-      <ul className='md:flex hidden'>
+      <ul className='md:flex hidden font-semibold text-gray-700'>
         <li className='p-2'>Home</li>
         <li className='p-2'>About</li>
         <li className='p-2'>Menu</li>
@@ -15,21 +21,30 @@ const Navbar = () => {
         <li className='p-2'>Login</li>
       </ul>
 
-      <div className='block md:hidden'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke-width='1.5'
-          stroke='currentColor'
-          class='w-6 h-6'
-        >
-          <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-          />
-        </svg>
+      <div className='block md:hidden' onClick={clickMenu}>
+        {showMenu ? (
+          <img className='w-6 h-6' src={Close} alt='menu' />
+        ) : (
+          <img className='w-6 h-6' src={Hamburger} alt='menu' />
+        )}
+      </div>
+
+      <div
+        className={
+          showMenu
+            ? 'fixed h-full left-0 top-0 w-[50%] border-r font-semibold text-gray-300 p-4 bg-[#108f54] ease-in-out duration-500'
+            : 'fixed left-[-100%]'
+        }
+      >
+        <img src={Logo} alt='logo' className='m-4' />
+
+        <ul className='p-8'>
+          <li className='p-2'>Home</li>
+          <li className='p-2'>About</li>
+          <li className='p-2'>Menu</li>
+          <li className='p-2'>Reservations</li>
+          <li className='p-2'>Login</li>
+        </ul>
       </div>
     </div>
   );
